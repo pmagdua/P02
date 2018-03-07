d10f40a051bfe7cb0e8df780d45628fea02a6c3a
reizagabriel <35972487+reizagabriel@users.noreply.github.com>
2018-03-06 22:34:38 -0800

    Updated product page, padding, content

--- Updated Styleguide/js/hamburger.js
+++ Updated Styleguide/js/hamburger.js
@@ -1,5 +1,3 @@
-/* Code from https://www.w3schools.com/howto/howto_js_topnav_responsive.asp */
-
 /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
 function myFunction() {
     var x = document.getElementById("navTop");
